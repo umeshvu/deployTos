a32e1c6f0f90874d0382ece18154ee47d148eaa6
umesh <umeshvu@outlook.com>
2023-08-08 21:21:02 +0100

    changes for deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OneComponent } from './one/one.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'one', component: OneComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
